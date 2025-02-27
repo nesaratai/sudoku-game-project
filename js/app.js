a0f9141472646f4bba8c1b2226620d9688540a70
nesaratai <nesaratai@gmail.com>
2025-02-27 17:24:23 -0500

    check for the duplicate numbers

--- js/app.js
+++ js/app.js
@@ -24,25 +24,36 @@ let solution = [
     ['9', '8', '1', '2', '5', '6', '4', '3', '7']
 ];
 
+
 let slots = document.querySelectorAll('.sqr');
+let resetButton = document.getElementById('reset');
+
 
 // 2. generate the board using the array from above
 function genBoard() {
     let index = 0;
 
-    for (let row = 0; row < board.length; row++) {
-        for (let col = 0; col < board[row].length; col++) {
-            slots[index].textContent = board[row][col];
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            slots[index].textContent = board[i][j];
 
             // if the cell is empty make it editable
-            if (board[row][col] === '') {
+            if (board[i][j] === '') {
                 slots[index].contentEditable = 'true';
 
                 // make a function to restrict slots take numbers between 1 to 9 and if the input is other than 1 to 9 clear
-                slots[index].addEventListener('input', function() {
-                    let value = slots[index].textContent.trim();
+                slots[index].addEventListener('input', (event) => {
+                    let value = event.target.textContent.trim();
                     if (!/^[1-9]$/.test(value)) {
-                        slots[index].textContent = '';
+                        event.target.textContent = '';
+                        return;
+                    }
+
+                    // if the number in the slot is a duplicate number change the color
+                    if (duplicateSlot(value, i, j)) {
+                        event.target.classList.add('duplicate');
+                    } else {
+                        event.target.classList.remove('duplicate');
                     }
                 });
             }
@@ -50,28 +61,62 @@ function genBoard() {
             index++;
         }
     }
-}
-
+};
 genBoard();
 
-// check if the row and column has repeated numbers clears the slot.
 
 
+// 3. check if a slot number you entered is a duplicate number
+function duplicateSlot(currentvalue, i, j) {
+    // check rows of the board for duplicate numbers
+    for (let col = 0; col < 9; col++) {
+        if (col !== j && slots[i * 9 + col].textContent === currentvalue) {
+            return true;
+        }
+    }
+
+    // check columns of the board for duplicate number
+    for (let row = 0; row < 9; row++) {
+        if (row !== i && slots[row * 9 + j].textContent === currentvalue) {
+            return true;
+        }
+    }
+
+    // check 3x3 box for all the board for duplicate numbers
+    let startRow = Math.floor(i / 3) * 3;
+    let startCol = Math.floor(j / 3) * 3;
+    
+    for (let r = startRow; r < startRow + 3; r++) {
+        for (let c = startCol; c < startCol + 3; c++) {
+            if ((r !== i || c !== j) && slots[r * 9 + c].textContent === currentvalue) {
+                return true;
+            }
+        }
+    }
+};
 
 
+// 4. check solution for the board if the borad is solved correctly with no duplicate number then show msg you win.
 
-// 3. building the gameboard solution
 
-// 5. validate player inputs that should be between 1 to 9
+// 5. put timer for player how much time passed when the player started the game
 
-// 6. display error msgs for invalid and repeating numbers
+// 6. activate the reset button
+resetButton.addEventListener('click', resetBoard);
+// reset the board
+function resetBoard() {
+    let slotindex = 0
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            slots[slotindex].textContent = board[i][j];
+            if (board[i][j] === '') {
+                slots[slotindex].contentEditable = 'true';
+                slots[slotindex].textContent = '';
+            } 
 
-// 7. check game state 
+            slotindex++;
+        }
+    }
+};
 
-// 8. put timer for player how much time passed when the player started the game
-
-// 9. activate the reset button
-
-// 10. check if the game is solved correctly.
-
-// 11. display the winning msg and stop the game.
+//7. display the winning msg and stop the game.
